ac133ce0791719780a18ad51b851e7c1f0d86d40
V.Anh Tran <tranvietanh1991@gmail.com>
2016-08-30 18:30:47 +0700

    gitlabci docker image. elastic search.

--- alpine/gitlabci-docker-python/docker-build.sh
+++ alpine/gitlabci-docker-python/docker-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --pull -t tranvietanh1991/gitlab-ready-docker1.12-python2.7-compose1.7:latest .
+docker build --pull -t tranvietanh1991/gitlab-ready-docker1.12-rc-dind-python2.7-compose:latest .
